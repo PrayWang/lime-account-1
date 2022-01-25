e896c72c8f9f9f8a79d9ee9b5af9f1aeb65abc2c
PrayWang <346076741@qq.com>
2022-01-25 17:03:28 +0800

    添加EditTag 通用组件

---
+++ src/components/Money/EditTag.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">{{this.fieldName}}</span>
+      <input type="text" v-model="value" :placeholder="this.placeholder">
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component, Prop, Watch} from 'vue-property-decorator';
+
+@Component
+export default class Notes extends Vue {
+  value = '';
+
+  @Prop({required:true})fieldName!:string;
+  @Prop()placeholder?:string;
+  @Watch('value')
+  onValueChanged(value: string) {
+    this.$emit('update:value', value);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.notes {
+  border-radius: 10px;
+  font-size: 16px;
+  background: white;
+  display: flex;
+  padding-left: 16px;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 62px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,6 +1,10 @@
 <template>
  <Layout>
-   编辑标签
+   <div>
+     <Icon name="left"/>
+     <span>编辑标签</span>
+   </div>
+   <EditTag field-name="标签名" placeholder="请输入标签名"/>
  </Layout>
 </template>
 
@@ -8,8 +12,10 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import {tagListModel} from '@/models/tagListModel';
-
-@Component
+import EditTag from '@/components/Money/EditTag.vue';
+@Component({
+  components: {EditTag}
+})
 export default class EditLabel extends Vue {
   created(){
     const id = this.$route.params.id;
