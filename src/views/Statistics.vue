ecb95561ef68d7e766808f0cbb7a06015297a9e4
PrayWang <346076741@qq.com>
2022-02-12 20:16:14 +0800

    完成统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="tabsWrapper">
-    <Layout>
-      <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-      <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+  <Layout>
+    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+    <ol>
+      <li v-for="(group, index) in groupedList" :key="index">
+        <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
         <ol>
-          <li v-for="(group,index) in result" :key="index">
-            <h3 class="title">title</h3>
-            <ol>
-              <li v-for="item in group" :key="item.id" class="record">
-                <span>{{tagString(item.tags)}}</span>
-                <span  class="notes">{{item.notes}}</span>
-                <span>￥{{item.amount}}</span>
-              </li>
-            </ol>
+          <li v-for="item in group.items" :key="item.id"
+              class="record"
+          >
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{item.amount}} </span>
           </li>
         </ol>
-    </Layout>
-  </div>
+      </li>
+    </ol>
+  </Layout>
 </template>
-
 <script lang="ts">
 import Tabs from '@/components/Tabs.vue';
-import intervalList from '@/constants/intervalList';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import recordTypeList from '@/constants/recordTypeList';
+import dayjs from 'dayjs';
+import clone from '@/lib/clone';
 
 @Component({
   components: {Tabs}
@@ -33,24 +32,49 @@ export default class Statistics extends Vue {
   tagString(tags:Tag[]){
     return  tags.length === 0 ? '无': tags.join(',');
   }
+
+  beautify(string:string){
+    const day  = dayjs(string);
+    const now  = dayjs();
+    if(day.isSame(now,'day')){
+     return '今天'
+   }else if(day.isSame(now.subtract(1,'day'),'day')){
+     return '昨天'
+   }else if(day.isSame(now.subtract(2,'day'),'day')){
+      return '前天'
+    }else if(day.isSame(now,'year')){
+      return day.format('M月D日')
+    }else{
+      return day.format('YYYY年M月D日')
+    }
+  }
+
   get recordList(){
     return (this.$store.state as RootState).recordList;
   }
 
-  get result(){
+  get groupedList() {
     const {recordList} = this;
-    // type HashTableValue = {title:string,items: RecordList[]}
-    // const hashTable : {[key:string] : HashTableValue} = {};
-    const hashTable:{[key:string]:RecordItem[]}={};
-    for(let i=0;i<recordList.length;i++){
-     const [date,time] = recordList[i].createdAt!.split('T');
-     console.log(date);
-     hashTable[date] = hashTable[date] || []
-      // hashTable[date].items.push({recordList[i]})
-      hashTable[date].push(recordList[i]);
+    if (recordList.length === 0) {return [];}
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+    type Result = { title: string, total?: number, items: RecordItem[] }[]
+    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+        last.items.push(current);
+      } else {
+        result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]});
+      }
     }
-    console.log(hashTable);
-    return hashTable;
+    result.map(group=>{
+      group.total = group.items.reduce((sum,item)=>sum + item.amount,0)
+    })
+
+    return result;
   }
 
   beforeCreate(){
@@ -58,8 +82,6 @@ export default class Statistics extends Vue {
   }
 
   type = '-';
-  interval = 'day';
-  intervalList = intervalList;
   recordTypeList = recordTypeList;
 };
 </script>
